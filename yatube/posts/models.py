c01915fe0f8470c0143796483d1f17105f7ec935
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-01 12:37:09 +0300

    1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -29,5 +29,8 @@ class Post(models.Model):
         on_delete=models.SET_NULL
     )
 
+    def __str__(self) -> str:
+        return self.text
+        
     class Meta:
         ordering = ['-pub_date']
