7a688d83e06d7966774fbbb27826445bfdfb6087
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-03 21:15:37 +0300

    1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,21 @@
+from django.shortcuts import render
+
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
 from django.contrib.auth.decorators import login_required
 
+from django.core.paginator import Paginator
+
 def index(request):
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
+        'posts': posts
     }
     return render(request, 'posts/index.html', context)
 
