2b06db318ae5c32d151be036c0b5924441abe460
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-04 15:23:57 +0300

    1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.conf import settings
-from django.views.generic.base import TemplateView
 
 def index(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, settings.LIMIT_POSTS)
+    paginator = Paginator(post_list, settings.MAX_RECORDS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
@@ -19,7 +18,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.all()
-    paginator = Paginator(post_list, settings.LIMIT_POSTS)
+    paginator = Paginator(post_list, settings.MAX_RECORDS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
@@ -27,3 +26,30 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    post_list = Post.objects.filter(author=author_user)
+    author_user = get_object_or_404(User, username=username)
+    paginator = Paginator(post_list, settings.MAX_RECORDS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    number_of_post = post_list.count()
+    context = {
+        'post_list': post_list,
+        'page_obj': page_obj,
+        'post_number': number_of_post,
+        'author': author_user,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post_number = Post.objects.select_related('author').filter(author=posts.author).count
+    posts = get_object_or_404(Post,pk=post_id)
+    context = {
+        'posts': posts,
+        'post_number': post_number,
+
+    }
+    return render(request, 'posts/post_detail.html', context) 
